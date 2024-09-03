cd7e212d2331d89fa40427854f8dfbf79833b6ea
Anton Maksimau <a.maksimov9876@gmail.com>
2024-09-03 15:42:52 -0400

    Added recognizing all the animals in the photo

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs').promises;
 const xml2js = require('xml2js');
+const path = require('path');
 
-const directoryPath = '../Dataset/annotations';
-
-function extractNameFromXML(filePath) {
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            console.error(`Error while reading file ${filePath}:`, err);
-            return;
-        }
-
+async function extractNameFromXML(filePath) {
+    try {
+        const data = await fs.readFile(filePath);
         const parser = new xml2js.Parser();
-        parser.parseString(data, (err, result) => {
-            if (err) {
-                console.error(`Error during parsing file ${filePath}:`, err);
-                return;
-            }
 
-            if (result.annotation && result.annotation.object) {
-                result.annotation.object.forEach(obj => {
-                    if (obj.name && obj.name[0]) {
-                        console.log(`File: ${filePath}, Name: ${obj.name[0]}`);
+        return new Promise((resolve, reject) => {
+            parser.parseString(data, (err, result) => {
+                if (err) {
+                    reject(`Error during parsing file ${filePath}: ${err}`);
+                }
+
+                if (result.annotation && result.annotation.object) {
+                    for (const obj of result.annotation.object) {
+                        if (obj.name && obj.name[0]) {
+                            resolve(obj.name[0]);
+                        }
                     }
-                });
-            }
+                }
+
+                resolve(null);
+            });
         });
-    });
+    } catch (err) {
+        console.error(`Error while reading file ${filePath}: ${err}`);
+    }
 }
 
+async function main() {
+    for (let i = 0; i < 3686; i++) {
+        const fileName = `Cats_Test${i}`;
+        const fileImage = `${fileName}.jpg`;
+        const filePath = path.join('../AI-training/Dataset/annotations', `${fileName}.xml`);
 
+        try {
+            const AnimalName = await extractNameFromXML(filePath);
+            console.log(`${fileImage} - ${AnimalName}`);
+
+            // AI Code
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+}
+
+main();
 
