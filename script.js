08b4ae2eb8898e1d96c71c7a7bba7781d93292e2
Anton Maksimau <a.maksimov9876@gmail.com>
2024-09-03 15:14:28 -0400

    Added dataset and started writing code

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const xml2js = require('xml2js');
 
-const directoryPath = 'C:/Users/pc/WebstormProjects/AI-training/Dataset/annotations';
+const directoryPath = '../Dataset/annotations';
+
+
 
