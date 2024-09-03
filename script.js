332514b785e58e3ff5cdefe5ff3f0f4a453b402a
Anton Maksimau <a.maksimov9876@gmail.com>
2024-09-03 15:29:01 -0400

    Added function "extractNameFromXML"

--- script.js
+++ script.js
@@ -4,5 +4,30 @@ const xml2js = require('xml2js');
 
 const directoryPath = '../Dataset/annotations';
 
+function extractNameFromXML(filePath) {
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error(`Error while reading file ${filePath}:`, err);
+            return;
+        }
+
+        const parser = new xml2js.Parser();
+        parser.parseString(data, (err, result) => {
+            if (err) {
+                console.error(`Error during parsing file ${filePath}:`, err);
+                return;
+            }
+
+            if (result.annotation && result.annotation.object) {
+                result.annotation.object.forEach(obj => {
+                    if (obj.name && obj.name[0]) {
+                        console.log(`File: ${filePath}, Name: ${obj.name[0]}`);
+                    }
+                });
+            }
+        });
+    });
+}
+
 
 
